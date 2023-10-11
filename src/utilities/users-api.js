c3276bb65ce9e54adab83d4e14c7fd825c65cee5
nadra10 <125341985+nadra10@users.noreply.github.com>
2023-10-11 13:39:24 -0400

    updated

--- src/utilities/users-api.js
+++ src/utilities/users-api.js
@@ -1,11 +1,35 @@
+
+
 import sendRequest from './send-request';
 
-const BASE_URL = 'http://localhost:3001/api/users';
+const BASE_URL = "http://localhost:3001/api/users";
 
-export function signUp(userData) {
-  return sendRequest(BASE_URL, 'POST', userData);
+
+
+export const signUp = async function (userData) {
+ 
+  return sendRequest(BASE_URL, "POST", userData);
+};
+
+export async function login(credentials) {
+
+  return sendRequest(`${BASE_URL}/login`, "POST", credentials);
 }
 
-export function login(credentials) {
-  return sendRequest(`${BASE_URL}/login`, 'POST', credentials);
-}
+export const checkToken = function () {
+  return sendRequest(`${BASE_URL}/check-token`);
+};
+
+
+
+// import sendRequest from './send-request';
+
+// const BASE_URL = 'http://localhost:3001/api/users';
+
+// export function signUp(userData) {
+//   return sendRequest(BASE_URL, 'POST', userData);
+// }
+
+// export function login(credentials) {
+//   return sendRequest(`${BASE_URL}/login`, 'POST', credentials);
+// }
